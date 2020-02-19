e932dc7e9c7665b061b39621d3ae45da121052e2
Win Naing <wintunnaing55@gmail.com>
2020-02-18 22:34:22 -0800

    Updates

---
+++ precache-manifest.43c7586591a0c600a52fe29251943b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ef140be74161bead28c976b0946685",
+    "url": "/Recipes_Search_React_App/index.html"
+  },
+  {
+    "revision": "711fd2ab0fced6278584",
+    "url": "/Recipes_Search_React_App/static/css/main.cd23957a.chunk.css"
+  },
+  {
+    "revision": "1997de1a696712a72c8c",
+    "url": "/Recipes_Search_React_App/static/js/2.663b31ec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Recipes_Search_React_App/static/js/2.663b31ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711fd2ab0fced6278584",
+    "url": "/Recipes_Search_React_App/static/js/main.7f5c2741.chunk.js"
+  },
+  {
+    "revision": "041ac7c6a7d8dc0ddd16",
+    "url": "/Recipes_Search_React_App/static/js/runtime-main.6ff56e01.js"
+  }
+]);
